2968f5220e6e0759bc5c4b687e237c0c914fdf99
XuanSang2005 <sangbom2005@gmail.com>
2025-09-14 18:17:10 +0700

    Create multipage pages using reaact-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { HomePage } from './pages/Homepage'
+import { Routes, Route } from 'react-router'
 
 function App() {
 
   return (
-    <>
-      <HomePage></HomePage>
-    </>
+      <Routes>
+        <Route path = "/" element = {<HomePage/>} /> 
+        <Route path = "checkout" element = {<div>Test checkout page</div>} /> 
+      </Routes>
   )
 }
 
